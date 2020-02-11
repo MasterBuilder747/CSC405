eb8865983a37692e29ae68efa8b0d6f8ceb7a9b2
Joseph <jaathunder@yahoo.com>
2020-02-11 15:30:47 -0800

    add and merge hw3 into lecture for 2d drawing

---
+++ src/Lecture/Week04/SceneGraph.java
@@ -0,0 +1,16 @@
+package Lecture.Week04;
+
+public class SceneGraph {
+
+    private double[][] scene = {
+            {-100,  100, 100,   -100},
+            {-100, -100, 100,    100},
+            {0,       0,   0,      0},
+            {1,       1,   1,      1}
+    };
+
+    public void render(int[][] framebuffer) {
+
+    }
+
+}
